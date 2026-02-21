a3430979736697bca61cad7341595503bf5ce631
dwgx <143298346+dwgx@users.noreply.github.com>
2026-02-21 19:08:43 +0900

    module: drop disabler from builtin registry

--- src/client/module/ModuleRegistry.java
+++ src/client/module/ModuleRegistry.java
@@ -1,15 +1,14 @@
 package client.module;
 
+import client.module.impl.client.AccountManagerModule;
 import client.module.impl.client.ClickGuiModule;
 import client.module.impl.client.HudEditModule;
 import client.module.impl.client.UiScaleEditModule;
 import client.module.impl.combat.AutoClickerModule;
-import client.module.impl.combat.KillAuraModule;
 import client.module.impl.misc.InventoryMoveModule;
+import client.module.impl.misc.SkinChangerModule;
 import client.module.impl.movement.EagleModule;
 import client.module.impl.movement.KeepSprintModule;
-import client.module.impl.movement.ScaffoldModule;
-import client.module.impl.render.TargetPanelModule;
 import client.module.impl.world.AutoToolModule;
 import client.module.impl.world.StealerModule;
 
@@ -30,16 +29,15 @@ public final class ModuleRegistry
         }
 
         modules.register(new ClickGuiModule());
+        modules.register(new AccountManagerModule());
         modules.register(new HudEditModule());
         modules.register(new UiScaleEditModule());
-        modules.register(new KillAuraModule());
         modules.register(new AutoClickerModule());
         modules.register(new EagleModule());
         modules.register(new KeepSprintModule());
-        modules.register(new ScaffoldModule());
         modules.register(new AutoToolModule());
         modules.register(new StealerModule());
+        modules.register(new SkinChangerModule());
         modules.register(new InventoryMoveModule());
-        modules.register(new TargetPanelModule());
     }
 }
